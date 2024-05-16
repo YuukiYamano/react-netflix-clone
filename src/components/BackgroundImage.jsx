40dbb84c50e8605e721544b9e34d5939546a7338
YuukiYamano <yuuki1108@gmail.com>
2024-05-17 00:57:12 +0900

    add project files& folders

---
+++ src/components/BackgroundImage.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import background from "../assets/login.jpg"
+import styled  from 'styled-components'
+
+export default function BackgroundImage() {
+  return (
+    <div>
+        <img src="{background}" alt="backgrund" />
+    </div>
+  )
+}
+
+const Container = styled.div`
+    height: 100vh;
+    width: 100vh;
+    img{
+        he
+        
+    }
+`
